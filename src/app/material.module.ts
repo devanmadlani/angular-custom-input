00ce2eeefc8a606df389fb1b796ae7ebffddc2bf
Devan Madlani <d.madlani@linx-it.nl>
2023-11-17 15:44:06 +0100

    refactor code

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
-
-const modules = [
-    MatInputModule,
-]
+const modules = [MatInputModule, MatButtonModule];
 
 @NgModule({
-    imports: [
-        ...modules
-    ],
-    exports: [
-        ...modules
-    ]
+  imports: [...modules],
+  exports: [...modules],
 })
-export class MaterialModule { }
+export class MaterialModule {}
